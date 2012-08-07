390d37ec6df87bbbbdaf8ab0c41326389604260c
malleor <malleor@gmail.com>
2012-08-08 00:34:44 +0200

    Changed plotting to use sequence objects.
    Fixed plotting.
    Fixed sequence getters.
    Flattened analog data being stored for easier plotting/processing.
    Added some docstrings.
    Refactored this'n'that.

--- pig_proc.py
+++ pig_proc.py
@@ -7,12 +7,16 @@ import numpy
 from copy import deepcopy
 
 class ForcePlates(object):
+	''' Information related to force plates measurement. '''
+	
 	def __init__(self, group):
 		self.num_plates = abs(struct.unpack('h', group.params['USED'].bytes)[0])
+		
 		# corners
 		corners_param = group.params['CORNERS']
 		corners_flat = struct.unpack('f'*3*4*self.num_plates, corners_param.bytes)
 		self.corners = numpy.reshape(corners_flat, (self.num_plates, 4, 3))
+		
 		# origin
 		origin_param = group.params['ORIGIN']
 		origin_flat = struct.unpack('f'*3*self.num_plates, origin_param.bytes)
@@ -27,9 +31,11 @@ class ForcePlates(object):
 		colors = map(lambda x: x, 'rbgcmyk'[::-1])
 		for i in xrange(self.num_plates):
 			c = colors.pop()
+			
 			# corners
 			x, y, z = zip(*self.corners[i])
 			a.scatter(x, y, z, c=c)
+			
 			# origin
 			x, y, z = self.origin[i]
 			a.scatter(x, y, z, c=c)
@@ -52,12 +58,17 @@ class Sequence(object):
 
 
 class C3DContent(object):
+	''' A c3d file content. Contains 3-D and scalar sequences 
+	along with metadata considering the measurement. '''
+	
 	def __init__(self, path):
 		import logging
 		
 		log = logging.FileHandler(path+'_read.log', 'wt')
 		log.setLevel(logging.DEBUG)
 		logging.root.addHandler(log)
+		
+		get_param_group = lambda name: self.groups.get(name.upper(), None)
 		
 		# load the c3d
 		self.file = path
@@ -70,14 +81,14 @@ class C3DContent(object):
 		self.groups = deepcopy(self._reader._groups)
 		
 		# get video labels
-		labels_raw = self.getgroup('POINT').params['LABELS']
+		labels_raw = get_param_group('POINT').params['LABELS']
 		label_len, num_markers = labels_raw.dimensions
 		extract_label = lambda i: labels_raw.bytes[label_len*i:label_len*(i+1)].strip(' ')
 		mask_non_markers = lambda label: label if C3DContent._is_marker(label) else None
 		video_labels = [mask_non_markers(extract_label(i)) for i in xrange(num_markers)]
 		
 		# get video labels
-		labels_raw = self.getgroup('ANALOG').params['LABELS']
+		labels_raw = get_param_group('ANALOG').params['LABELS']
 		label_len, num_markers = labels_raw.dimensions
 		extract_label = lambda i: labels_raw.bytes[label_len*i:label_len*(i+1)].strip(' ')
 		analog_labels = [extract_label(i) for i in xrange(num_markers)]
@@ -87,7 +98,7 @@ class C3DContent(object):
 		num_video_sequences = video[0].shape[0]
 		num_analog_sequences = analog[0].shape[1]
 		make_video_sequence = lambda i: Sequence([f[i] for f in video], video_labels[i])
-		make_analog_sequence = lambda i: Sequence([f[:,i] for f in analog], analog_labels[i])
+		make_analog_sequence = lambda i: Sequence(numpy.append([], [f[:,i] for f in analog]), analog_labels[i])
 		self.video = [make_video_sequence(i) for i in xrange(num_video_sequences) if video_labels[i]]
 		self.analog = [make_analog_sequence(i) for i in xrange(num_analog_sequences)]
 		logging.info('Extracted video sequences:')
@@ -96,27 +107,29 @@ class C3DContent(object):
 		for s in self.analog: logging.info('  ' + str(s))
 		
 		# get force plates info
-		fp_group = self.getgroup('FORCE_PLATFORM')
+		fp_group = get_param_group('FORCE_PLATFORM')
 		self.force_plates = ForcePlates(fp_group)
 		
 		logging.root.removeHandler(log)
-	
-	def getgroup(self, name):
-		return self.groups.get(name.upper(), None)
 
-	def plot(self, marker_index_or_label, fig=None, limits=None, mstyle=None):
-		'''Plots a video signal of given its index or label.'''
-		# TODO: refactor to use sequence objects
-		label, marker = self.getmarker(marker_index_or_label)
-		if limits is not None:
-			marker = marker[limits[0]:limits[1]]
-		plt.figure(fig or 'Marker '+label)
-		for i in xrange(4):
-			plt.subplot(4, 1, i+1)
+	def plot(self, label, fig=None, limits=None, mstyle=None):
+		''' Plots a video signal of given its index or label. '''
+		
+		sequence = self.find_video(label) or self.find_analog(label)
+		if not sequence: 
+			raise ValueError('sequence not found')
+		array = sequence.array[limits[0]:limits[1]] if limits else sequence.array
+		
+		plt.figure(fig or 'Sequence \'%s\'' % sequence.name)
+		dims = array.shape[1] if array.ndim > 1 else 1
+		for i in xrange(dims):
+			plt.subplot(dims, 1, i+1)
 			plt.cla()
-			plt.plot([v[i] for v in marker], marker=mstyle)
+			plt.plot([v[i] for v in array] if array.ndim > 1 else array, marker=mstyle)
 	
 	def save(self, path):
+		''' Save the content to a c3d file. '''
+		
 		# TODO: refactor to use sequence objects
 		raise NotImplementedError('\'save\' function is currently under construction')
 		
@@ -137,22 +150,27 @@ class C3DContent(object):
 		handle.flush()
 		
 		# write video and analog data
-		frames = zip(self.video, self.analog)
+		# TODO: video, analog = ...
+		frames = zip(video, analog)
 		writer.write_frames(frames)
 		
 		logging.root.removeHandler(log)
 
-	def getmarker(self, label):
-		'''Retreives a sequence of a specific video signal.'''
-		
-		# TODO: select sequence
-		raise NotImplementedError('\'getmarker\' function is currently under construction')
-		
-		return (sequence.name, sequence.array)
+	def find_video(self, label):
+		'''Retreives a video sequence of a specific name.'''
+		try:
+			is_good_name = C3DContent._label_matches(label)
+			return filter(lambda v: is_good_name(v.name), self.video)[0]
+		except IndexError:
+			return None
 
-	def getmarkers(self):
-		for index in video_labels.itervalues():
-			yield self.getmarker(index)
+	def find_analog(self, label):
+		'''Retreives an analog sequence of a specific name.'''
+		try:
+			is_good_name = C3DContent._label_matches(label)
+			return filter(lambda v: is_good_name(v.name), self.analog)[0]
+		except IndexError:
+			return None
 	
 	@staticmethod
 	def _label_matches(base_label):
@@ -183,6 +201,10 @@ def save(content, dirpath, limits=None, separate=True, markers=None):
 	separate  - write each trajectory into a separate file? (default: yes)
 	markers   - list of markers to be written (default: all)
 	'''
+	
+	# TODO: refactor to use sequence objects
+	raise NotImplementedError('\'save\' function is currently under construction')
+	
 	if not os.path.isdir(dirpath):
 		dirpath = os.path.split(dirpath)[0]
 	
